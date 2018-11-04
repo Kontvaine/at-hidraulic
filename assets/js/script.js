45b44afe7516ebb9e761a99ea8dfc2674e7b7ac3
Agle <egle.kontvaine@gmail.com>
2018-11-04 18:17:50 +0200

    change hover and JS code for resposive layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	
+	if ( $(window).width() > 1024){ 
+	console.log("didelis");
 	$(".item").hover(function() {
     $(this).siblings().toggleClass("item--smaller");
 		 $(this).toggleClass("item--wider");
@@ -8,5 +10,9 @@ $(document).ready(function(){
 		
 );
 
+	}
+
 });
+	
+	
 
