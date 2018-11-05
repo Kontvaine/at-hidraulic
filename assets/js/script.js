b14516ae3c3c150b03de5cf807b717226d0fd2da
Agle <egle.kontvaine@gmail.com>
2018-11-05 15:16:59 +0200

    recent editions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-$(document).ready(function(){
-	
-	if ( $(window).width() > 1024){ 
-	console.log("didelis");
-	$(".item").hover(function() {
-    $(this).siblings().toggleClass("item--smaller");
-		 $(this).toggleClass("item--wider");
-}, 
-   
-		
-);
 
-	}
-
-});
-	
 	
 
